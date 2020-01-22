4bb84bf0f6e088a38654d99dc6599561bb715110
Gabriel Ruivo <gabrielruivo@MacBook-Pro-de-Gabriel.local>
2020-01-22 13:57:59 -0300

    mudança js

--- index.js
+++ index.js
@@ -3,12 +3,4 @@ let btnDownload = document.querySelector(".btn-dwn-cv");
 
 function imprimirCv () {
     window.print()
-}
-
-    const pdfUrl = "download/teste.txt";
-
-    const downloadName = "cv.txt";
-
-    btnDownload.url = pdfUrl;
-
-    btnDownload.download = downloadName;
+};
